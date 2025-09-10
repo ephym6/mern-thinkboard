c2ccc796277c005ef50e67984ee6e35305e53cab
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-10 11:34:30 +0300

    Testing an api with a get request

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/api/notes', (req, res) => {
+    res.send('you got 1 note');
+});
+
 app.listen(5001, () => {
     console.log('Server is running on PORT 5001');
 })
