e72a84f94bd685416422b1b05827214a2e8a3369
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-11 12:05:58 +0300

    Added api endpoints

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,20 @@ import express from 'express';
 const app = express();
 
 app.get('/api/notes', (req, res) => {
-    res.status(200).send('you got 1 note');
+    res.status(200).send('you got 3 note');
 });
+
+app.post('/api/notes', (req, res) => {
+    res.status(201).json({ message: 'Note created successfully' });
+})
+
+app.put('/api/notes/:id', (req, res) => {
+    res.status(201).json({ message: 'Note updated successfully' });
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    res.status(201).json({ message: 'Note deleted successfully' });
+})
 
 app.listen(5001, () => {
     console.log('Server is running on PORT 5001');
