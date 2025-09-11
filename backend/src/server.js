d0b34f2e0c5d3614ad91c4383082dda774868c46
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-11 13:09:07 +0300

    Exported connectDB function

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import connectDB from "./config/db.js";
 
 const app = express();
+
+connectDB();
 
 app.use('/api/notes', notesRoutes);
 
 app.listen(5001, () => {
     console.log('Server is running on PORT 5001');
 })
+
+
+
+
+// mongodb+srv://ephym:<db_password>@cluster0.g9bmm85.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
