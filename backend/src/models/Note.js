cae722e033635325130842d4eb30097ba2c85dee
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-11 13:24:17 +0300

    Created Note model & defined note schema

---
+++ backend/src/models/Note.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+// Define the Note schema
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true} // Automatically manage createdAt and updatedAt fields
+);
+
+// Create the Note model
+const Note = mongoose.model('Note', noteSchema);
+
+export default Note;
