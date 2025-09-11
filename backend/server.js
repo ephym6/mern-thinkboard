ce9823ab316d673b255277e126e00f316c473717
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-11 12:13:56 +0300

    Updates server.js with main api route

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,9 @@
 import express from 'express';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 
-app.get('/api/notes', (req, res) => {
-    res.status(200).send('you got 3 note');
-});
-
-app.post('/api/notes', (req, res) => {
-    res.status(201).json({ message: 'Note created successfully' });
-})
-
-app.put('/api/notes/:id', (req, res) => {
-    res.status(201).json({ message: 'Note updated successfully' });
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-    res.status(201).json({ message: 'Note deleted successfully' });
-})
+app.use('/api/notes', notesRoutes);
 
 app.listen(5001, () => {
     console.log('Server is running on PORT 5001');
