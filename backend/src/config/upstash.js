11ba86e55a317979a1173af3a4277b30d74b9265
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-25 18:14:25 +0300

    Setup upstash rate limit & redis

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,17 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from '@upstash/redis'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+const ratelimit = new Ratelimit({
+    redis: redis,
+    limiter: Ratelimit.slidingWindow(100, "60 s"), // 5 requests per 20 seconds
+})
+
+export { ratelimit };
