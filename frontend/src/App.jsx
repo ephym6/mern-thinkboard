f98505dbfac6566be4056b406d3f87e246a4476b
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-25 18:50:51 +0300

    Added page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Route, Routes } from "react-router";
+import HomePage from "./page/HomePage.jsx";
+import CreatePage from "./page/CreatePage.jsx";
+import NoteDetailPage from "./page/NoteDetailPage.jsx";
 
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
   );
 };
