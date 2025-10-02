5cf25693d06eeef8cdadd1f3e0c90cc79598afe1
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-02 17:19:06 +0300

    Added border on card

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -6,7 +6,8 @@ import { formatDate } from "../lib/utils.js";
 const NoteCard = ({note}) => {
   return (
     <Link to={`/note/${note._id}`}
-            className="bg-base-200 p-4 rounded-lg shadow hover:shadow-lg transition-shadow duration-300">
+            className="card bg-base-100 hover:shadow-lg transition-all duration-200
+      border-t-4 border-solid border-[#00FF9D]">
         <div className="card-body">
             <h3 className='card-title text-base-content'>{note.title}</h3>
             <p className='text-base-content/70 line-clamp-3'>{note.content}</p>
