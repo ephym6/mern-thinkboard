08e1bd62e1212860842e27157f14a361518263b1
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-02 15:46:51 +0300

    Updated error response

--- frontend/src/page/HomePage.jsx
+++ frontend/src/page/HomePage.jsx
@@ -21,7 +21,7 @@ const HomePage = () => {
                 )
             } catch (error) {
                 console.log('Error fetching notes:', error);
-                if (error.response.status === 429) {
+                if (error.response?.status === 429) {
                     setIsRateLimited(true);
                 } else {
                     toast.error('Failed to load notes. Please try again later.');
