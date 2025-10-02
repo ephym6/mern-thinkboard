56ea2b3b89e950df51dadb46f685c79efe7aa4d5
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-02 15:40:15 +0300

    Updated notes, ratelimit and loading states

--- frontend/src/page/HomePage.jsx
+++ frontend/src/page/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import RateLimitedUI from "../components/RateLimitedUI.jsx";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
     const [isRateLimited, setIsRateLimited] = useState(true);
@@ -13,8 +14,20 @@ const HomePage = () => {
             try {
                 const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/notes`);
                 console.log(res.data);
+                setNotes(res.data);
+                setIsRateLimited(false);
+                console.log(
+                    `Fetched ${notes.length} notes from the API`
+                )
             } catch (error) {
                 console.log('Error fetching notes:', error);
+                if (error.response.status === 429) {
+                    setIsRateLimited(true);
+                } else {
+                    toast.error('Failed to load notes. Please try again later.');
+                }
+            } finally {
+                setLoading(false);
             }
         }
 
