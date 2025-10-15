1304d2c7c224e368d16238b672f1b0423e830807
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-15 16:41:52 +0300

    Added error handling to db connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,7 +8,9 @@ const connectDB = async () => {
       await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.g9bmm85.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority&appName=Cluster0`)
         console.log("MongoDB Connected Successfully")
   } catch (error) {
-        console.error("MongoDB connection failed", error);
+        console.error("MongoDB connection failed");
+        console.error("Error details: ", error.message);
+        if (error.code) console.error("Error code: ", error.code);
         process.exit(1); // Exit process with failure
   }
 };
