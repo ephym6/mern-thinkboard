be6370792af6d405ae531911dbc8e25ae7579690
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-28 13:21:05 +0300

    Update axios instance to include auth token & wrap app with AuthProvider

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { auth } from '../config/firebase.js';
 
 // in production, there's no localhost so we have to make this dynamic
 const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_API_URL}/api` : "/api";
@@ -6,5 +7,20 @@ const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VIT
 const api = axios.create({
     baseURL: BASE_URL,
 })
+
+// Add token to requests
+api.interceptors.request.use(
+    async (config) => {
+        const user = auth.currentUser;
+        if (user) {
+            const token = await user.getIdToken();
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
