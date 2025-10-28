7f92bc4603d7a6b39c18af265950b322eb8d541b
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-28 13:16:03 +0300

    Protect notesRoutes with auth middleware

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import {createNote, deleteNote, getAllNotes, getNoteById, updateNote} from "../controllers/notesController.js";
+import { authenticateUser } from '../middleware/auth.js';
 const router = express.Router();
 
-router.get('/', getAllNotes);
+router.get('/', authenticateUser, getAllNotes);
 
-router.get('/:id', getNoteById);
+router.get('/:id', authenticateUser, getNoteById);
 
-router.post('/', createNote);
+router.post('/', authenticateUser, createNote);
 
-router.put('/:id', updateNote);
+router.put('/:id', authenticateUser, updateNote);
 
-router.delete('/:id', deleteNote);
+router.delete('/:id', authenticateUser, deleteNote);
 
 export default router;
